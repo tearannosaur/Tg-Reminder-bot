b42fa845c98db1dd0377259c8a04c853f0047816
Ivan <vaniarom15@gmail.com>
2026-02-11 15:23:56 +0300

    save user feat

--- repository/save_users.go
+++ repository/save_users.go
@@ -4,6 +4,11 @@ import (
 	"reminder/models"
 )
 
-func (r *ReposytoryModule) SaveUsers(user models.Users) {
-
+func (r *RepositoryModule) SaveUsers(user models.Users) error {
+	query := `INSERT INTO users (id,nick_name,first_name,last_name) VALUES ($1,$2,$3,$4)`
+	_, err := r.db.Exec(query, user.Id, user.NickName, user.FirstName, user.LastName)
+	if err != nil {
+		return err
+	}
+	return nil
 }
